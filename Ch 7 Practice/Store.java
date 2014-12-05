17b6f2808bbffc6be0605e8c7b3b84588bfe0a0b
ikbiel <ikbiel13@gmail.com>
2014-12-05 15:08:41 -0600

    Practice lab

--- Ch 7 Practice/Store.java
+++ Ch 7 Practice/Store.java
@@ -4,30 +4,69 @@ import java.util.Scanner;
 
 public class Store
 {
+    private ArrayList<String> customers;
+    private ArrayList<Double> prices;
+    public Scanner s;
+    
+    
     public Store()
     {
-       ArrayList<String> customers = new ArrayList<String>();
-       ArrayList<Double> prices = new ArrayList<Double>(); 
-       Scanner s = new Scanner(System.in);
+       customers = new ArrayList<String>();
+       prices = new ArrayList<Double>(); 
+       s = new Scanner(System.in);
+       
+       
     }
 
-    public void addSale(String cutomerName, double amount)
+    public void addSale(String customerName, double amount)
     {
-       System.out.println("Enter the customer's name: ");
-       customerName = s.next();
-       customers.add(customerName);
        
+           customers.add(customerName);
+       
+           prices.add(amount);
+
        
     }
     
     public String nameOfBestCustomer()
     {
+        int max = 0;
+        String bestCustomer = "";
+        for( int i = 0; i < customers.size(); i++ )
+        {
+            if( prices.get(i) > max )
+            {
+                bestCustomer = customers.get(i);
+            }
+        }
+        
+        return bestCustomer;
         
     }
     
     public static void main(String[] args)
     {
+        Store myStore = new Store();
         
-    }
+        System.out.println("Enter the customer's name (enter 'q' if no more customers): ");
+        customerName = s.next();
+        while( s.hasNext() )
+        {
+           if( customerName == "q" )
+           {
+               break;
+           }
+       
+           System.out.println("Enter the customer's price : ");
+           amount = s.nextDouble();
+        }
+        
+        
+        
+         System.out.print(myStore.addSale());
+         System.out.println(myStore.nameOfBestCustomer());
+        }
+    } 
 
-}
+
+        
